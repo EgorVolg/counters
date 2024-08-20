8a5cdf98e6c34e4109241c2e135e74d7712f3c20
Valery <duna9588@gmail.com>
2024-08-20 14:20:00 +0300

    Revert "create custom hook useAction"
    This reverts commit 7d61a2268eca47b9e0d871c13f71d9a9ec67688d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import { useEffect } from "react";
 import { ItemsList } from "./components/Itemslist.tsx";
-import { updateAreas } from "./state/slices/areasSlices.ts";
-import { useAction } from "./components/hooks/useAction.ts";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import areasSlices  from "./state/slices/areasSlices.ts";
+import { TArea } from "./components/types.ts";
 
 export const App = () => {
-  const { dispatch } = useAction();
+  const areas = useSelector((state: TArea[]) => state );
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(updateAreas(data));
-  }, [dispatch]);
+    async function areasFetchData() {
+      const fetchRes = await axios.get("http://localhost:3000/areas");
+      
+      dispatch(areasSlices.updateAreas({areas: fetchRes.data }));
+    }
+    areasFetchData();
+  }, []);
+
   return (
     <div className="block bg-[#F8F9FA]">
       <header>
         <strong>Список счётчиков</strong>
       </header>
       <main className="m-3 rounded-[12px] border-[1px] border-solid border-[#E0E5EB]">
-        <ItemsList />
+        <ItemsList areas={areas} />
       </main>
     </div>
   );
--- src/components/Itemslist.tsx
+++ src/components/Itemslist.tsx
@@ -1,62 +1,70 @@
-import React, { useEffect, useState } from "react";
-import { TArea, TMeter } from "./types.ts";
-import { Item } from "./Item.tsx";
+import React from "react";
+import {TArea, TMeter} from "./types.ts";
+import {Item} from "./Item.tsx";
 import axios from "axios";
-import { useSelector } from "react-redux";
 
 type TProps = {
-  areas: TArea[];
-};
-export const ItemsList: React.FC<TProps> = () => {
-  const [meters, setMeters] = useState<TMeter[]>([]);
+    areas: TArea[]
+}
+export const ItemsList: React.FC<TProps> = ({areas}) => {
+    const [meters, setMeters] = React.useState<TMeter[]>([])
 
-  const areas = useSelector((state: TArea[]) => state);
+    React.useEffect(() => {
+        async function metersFetchData() {
+            const itemsRes = await axios.get(
+                "http://localhost:3000/meters"
+            )
+            setMeters(itemsRes.data);
+        }
+        metersFetchData();
+    }, []);
 
-  console.log(areas, "areas");
+     const navItemsList = [
+        {title: "№"},
+        {title: "Тип"},
+        {title: "Дата установки"},
+        {title: "Автоматический"},
+        {title: "Текущие показания"},
+        {title: "Адрес"},
+        {title: "Примечание"}
+    ]
 
-  useEffect(() => {
-    async function metersFetchData() {
-      const itemsRes = await axios.get("http://localhost:3000/meters");
-      setMeters(itemsRes.data);
-    }
-    metersFetchData();
-  }, []);
-
-  const navItemsList = [
-    { title: "№" },
-    { title: "Тип" },
-    { title: "Дата установки" },
-    { title: "Автоматический" },
-    { title: "Текущие показания" },
-    { title: "Адрес" },
-    { title: "Примечание" },
-  ];
-
-  const navItemsListMap = () => {
-    return navItemsList.map((str: { title: string }, index: number) => (
-      <div className="p-1 bg-[#F0F3F7] text-neutral-500" key={index}>
-        {str.title}
-      </div>
-    ));
-  };
-
-  const createMeter = meters.map((obj: TMeter, index: number) => {
-    const areaMap = areas.find((el: TArea) => el.id === obj.area.id);
-
-    if (areaMap) {
-      const newObj = { ...obj, ...areaMap };
-      return <Item el={newObj} key={index} />;
+    const navItemsListMap = () => {
+        return (
+            navItemsList.map((str: { title: string }, index: number) => (
+                <div
+                    className="p-1 bg-[#F0F3F7] text-neutral-500"
+                    key={index}
+                >
+                    {str.title}
+                </div>
+            ))
+        )
     }
 
-    return obj;
-  });
+    const createMeter = meters.map((obj: TMeter, index: number) => {
+            const areaMap = areas.find((el: TArea) => el.id === obj.area.id);
 
-  return (
-    <div>
-      <header className="grid grid-cols-[0.5fr_0.8fr_0.8fr_0.8fr_0.8fr_1.8fr_1.8fr] gap-[0px 0px] grid-flow-row">
-        {navItemsListMap()}
-      </header>
-      <main>{createMeter}</main>
-    </div>
-  );
-};
+            if (areaMap) {
+                const newObj = {...obj, ...areaMap}
+                return (
+                    <Item el={newObj} key={index}/>
+                )
+            }
+
+            return obj;
+        }
+    )
+
+    return (
+        <div>
+            <header
+                className="grid grid-cols-[0.5fr_0.8fr_0.8fr_0.8fr_0.8fr_1.8fr_1.8fr] gap-[0px 0px] grid-flow-row">
+                {navItemsListMap()}
+            </header>
+            <main>
+                {createMeter}
+            </main>
+        </div>
+    )
+}
