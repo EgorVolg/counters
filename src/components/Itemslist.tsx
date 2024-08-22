d7cc1ef5b5aea4b585376c06cf9e297a37831b66
Valery <duna9588@gmail.com>
2024-08-22 12:00:59 +0300

    fix hook useAreas
    fix problem with hook useAreas

--- src/components/Itemslist.tsx
+++ src/components/Itemslist.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-import { TMeter } from "./types.ts";
+import { TArea, TMeter } from "./types.ts";
 import axios from "axios";
-import { useAreas } from "./hooks/useAreas.js";
-import { NavItemsListMap } from "./NavItemsTopBar";
+ import { NavItemsListMap } from "./NavItemsTopBar";
+import { useAreas } from "./hooks/useAreas.ts";
+import { Item } from "./Item.tsx";
 
 export const ItemsList = () => {
   const [meters, setMeters] = React.useState<TMeter[]>([]);
@@ -18,23 +19,23 @@ export const ItemsList = () => {
   }, []);
   console.log(areas, "areas");
    
-  //   const createMeter = meters.map((obj: TMeter, index: number) => {
-  //     const areaMap = areas.find((el: TArea) => el.id === obj.area.id);
+    const createMeter = meters.map((obj: TMeter, index: number) => {
+      const areaMap = areas.find((el: TArea) => el.id === obj.area.id);
 
-  //     if (areaMap) {
-  //       const newObj = { ...obj, ...areaMap };
-  //       return <Item el={newObj} key={index} />;
-  //     }
+      if (areaMap) {
+        const newObj = { ...obj, ...areaMap };
+        return <Item el={newObj} key={index} />;
+      }
 
-  //     return obj;
-  //   });
+      return obj;
+    });
 
   return (
     <div>
       <header className="grid grid-cols-[0.5fr_0.8fr_0.8fr_0.8fr_0.8fr_1.8fr_1.8fr] gap-[0px 0px] grid-flow-row">
         <NavItemsListMap />
       </header>
-      {/* <main>{createMeter}</main> */}
+      <main>{createMeter}</main>
     </div>
   );
 };
--- src/components/hooks/useAreas.ts
+++ src/components/hooks/useAreas.ts
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux"
 import { TArea } from "../types"
 
 export const useAreas = () => {
-    const { areas } = useSelector((state:TArea[]) => state.areas)
-    return { areas }
-
+    const areas = useSelector((state: { areas: TArea[] }) => state.areas)
+    return {areas} 
 }
